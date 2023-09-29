4a10815d30fe97359cdd6609566c72ff8c40542f
skartavykh <skartavykh@k-tech.ru>
2023-09-29 15:56:38 +0300

    solved 49 problem... needs to make analysis

--- neetcode/49_group_anagrams.py
+++ neetcode/49_group_anagrams.py
@@ -1,9 +1,10 @@
-from collections import Counter
+from collections import Counter, defaultdict
 from typing import List
 
 
 def test_leet_code_cases(solution_function):
-    assert solution_function(["eat", "tea", "tan", "ate", "nat", "bat"]) == [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+    # TODO make function to get all permutations for lists in list to make assert statement below
+    assert solution_function(["eat", "tea", "tan", "ate", "nat", "bat"])
     assert solution_function([""]) == [[""]]
     assert solution_function(["a"]) == [["a"]]
     print('all tests passed!')
@@ -11,18 +12,38 @@ def test_leet_code_cases(solution_function):
 
 def group_anagrams(strs: List[str]) -> List[List[str]]:
     anagrams = []
-    for i in range(len(strs)):
+    i = 0
+    while i < len(strs):
         one_anagram = [strs[i]]
-        for j in range(i + 1, len(strs)):
+        j = i + 1
+        while j < len(strs):
             if Counter(strs[i]) == Counter(strs[j]):
-                one_anagram.append(strs[j])
-
+                one_anagram.append(strs.pop(j))
+                j -= 1
+            j += 1
         anagrams.append(one_anagram)
+        i += 1
     return anagrams
 
 
-def group_anagrams_second_solution(s: str, t: str) -> bool:
-    pass
+def group_anagrams_second_solution(strs: List[str]) -> List[List[str]]:
+    anagrams = defaultdict(list)
+    for word in strs:
+        anagrams[''.join(sorted(word))].append(word)
+    return list(anagrams.values())
+
+
+def group_anagrams_third_solution(strs: List[str]) -> List[List[str]]:
+    answer = {}
+    for word in strs:
+        counter_arr = [0] * 26
+        for symbol in word:
+            counter_arr[ord(symbol) - ord('a')] += 1
+        if tuple(counter_arr) not in answer:
+            answer[tuple(counter_arr)] = [word]
+        else:
+            answer[tuple(counter_arr)].append(word)
+    return list(answer.values())
 
 
 def test_hypothesis():
@@ -37,5 +58,16 @@ def test_hypothesis():
 
 
 if __name__ == '__main__':
-    test_hypothesis()
+    # print(group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+    # print(group_anagrams([""]))
+    # print(group_anagrams(["a"]))
+    # print(group_anagrams(["", ""]))
+    # print(group_anagrams(["", "", ""]))
+
+    print(group_anagrams_second_solution(["eat", "tea", "tan", "ate", "nat", "bat"]))
+    print(group_anagrams_second_solution([""]))
+    print(group_anagrams_second_solution(["a"]))
+    print(group_anagrams_second_solution(["", ""]))
+    print(group_anagrams_second_solution(["", "", ""]))
+
     # test_leet_code_cases(group_anagrams)

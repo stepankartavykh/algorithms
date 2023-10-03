f3b6e8cce48b4f5dc8a78848878c7c02bd2237be
skartavykh <skartavykh@k-tech.ru>
2023-10-03 08:28:09 +0300

    solved 238 problem.

--- neetcode/238_product_of_array_except_self.py
+++ neetcode/238_product_of_array_except_self.py
@@ -3,23 +3,29 @@ from typing import List
 
 def test_leet_code_cases(solution_function):
     assert solution_function([1, 2, 3, 4]) == [24, 12, 8, 6]
-    assert solution_function([-1, 1, 0, -3, 3], 1) == [0, 0, 9, 0, 0]
+    assert solution_function([-1, 1, 0, -3, 3]) == [0, 0, 9, 0, 0]
     print('all tests passed!')
 
 
 def product_of_array_except_self(nums: List[int]) -> List[int]:
     total_product = 1
-    zeros = set()
+    zeros = []
     for position, num in enumerate(nums):
-        total_product *= num
         if not num:
-            zeros.add(position)
-
+            zeros.append(position)
+        else:
+            total_product *= num
         if len(zeros) == 2:
             return [0] * len(nums)
-    answer = []
-    for num in nums:
-        answer_element = 1
+    length = len(zeros)
+    if length >= 2:
+        return [0] * len(nums)
+    elif length == 1:
+        arr = [0] * len(nums)
+        arr[zeros[0]] = total_product
+        return arr
+    else:
+        return [int(total_product / element) for element in nums]
 
 
 if __name__ == '__main__':

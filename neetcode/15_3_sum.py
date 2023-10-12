a4ace074407ac86a9c66b1b8dac60b37390f8d99
skartavykh <skartavykh@k-tech.ru>
2023-10-12 18:32:50 +0300

    15 - solved.

--- neetcode/15_3_sum.py
+++ neetcode/15_3_sum.py
@@ -1,13 +1,35 @@
 from typing import List
 
 
+def two_sum(nums: List[int], target: int) -> List[int]:
+    hash_map = {}
+    for index, num in enumerate(nums):
+        if target - num in hash_map:
+            return [hash_map[target - num], index]
+        hash_map[num] = index
+
+
 def three_sum(nums: List[int]):
+    triplets = []
     nums.sort()
-    left, right = 0, len(nums) - 1
-    while left < right:
-        while nums[left] + nums[right] > 0:
-            right -= 1
-        while nums[left] + nums[right] < 0:
-            left += 1
-        if nums[left] + nums[right] == 0:
-            return [left + 1, right + 1]
+    for index, num in enumerate(nums):
+        if index > 0 and num == nums[index - 1]:
+            continue
+
+        left, right = index + 1, len(nums) - 1
+        while left < right:
+            sum_ = num + nums[left] + nums[right]
+            if sum_ > 0:
+                right -= 1
+            elif sum_ < 0:
+                left += 1
+            else:
+                triplets.append([num, nums[left], nums[right]])
+                left += 1
+                while nums[left] == nums[left - 1] and left < right:
+                    left += 1
+    return triplets
+
+
+if __name__ == '__main__':
+    print(three_sum([-1, 0, 1, 2, -1, -4]))

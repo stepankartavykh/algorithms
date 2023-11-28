73100f6f9d9ef901ffb7b42c30cdaf2d8ac7898e
sklion <sktvpx@gmail.com>
2023-11-28 20:25:53 +0300

    11 - done.

--- neetcode/TwoPointers/11_container_with_most_water.py
+++ neetcode/TwoPointers/11_container_with_most_water.py
@@ -8,13 +8,15 @@ def max_area(height: List[int]) -> int:
     while left < right:
 
         amount = (right - left) * min(height[left], height[right])
-        if amount > max_amount:
-            max_amount = amount
-        left += 1
-        right -= 1
+        max_amount = max(amount, max_amount)
+        if height[left] <= height[right]:
+            left += 1
+        elif height[left] > height[right]:
+            right -= 1
 
     return max_amount
 
 
 if __name__ == '__main__':
     print(max_area([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+    print(max_area([7, 8, 6, 2, 5, 4, 8, 3, 7]))

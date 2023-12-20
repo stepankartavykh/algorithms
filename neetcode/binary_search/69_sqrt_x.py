1fb54671353687185fb9407507384fc81e722ff6
sklion <sktvpx@gmail.com>
2023-12-20 21:34:38 +0300

    69 - done

--- neetcode/binary_search/69_sqrt_x.py
+++ neetcode/binary_search/69_sqrt_x.py
@@ -1,18 +1,20 @@
 def my_sqrt(x: int) -> int:
     left, right = 0, x
+    result = 0
 
-    while left < right:
-        middle = (left + right) // 2
+    while left <= right:
+        middle = left + (right - left) // 2
         val = middle * middle
 
         if val > x:
             right = middle - 1
         elif val < x:
             left = middle + 1
+            result = middle
         else:
             return middle
 
-    return left
+    return result
 
 
 if __name__ == '__main__':

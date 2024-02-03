02eb93abcd5ea98bd9ed7eab6b9537146759b9fd
sklion <sktvpx@gmail.com>
2024-02-03 22:12:43 +0300

    105 - template for problem

---
+++ neetcode/Trees/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from neetcode import TreeNode, print_binary_tree_structure
+
+
+def build_tree(preorder: list[int], inorder: list[int]) -> Optional[TreeNode]:
+    # TODO preorder - node.val + traversal(left) + traversal(right)
+    # TODO inorder - traversal(left) + node.val + traversal(right)
+
+    return TreeNode(val=-1)
+
+
+if __name__ == '__main__':
+    inputs = [
+        ([3, 9, 20, 15, 7], [9, 3, 15, 20, 7]),
+        ([-1], [-1]),
+    ]
+    for input_ in inputs:
+        print(print_binary_tree_structure(build_tree(input_[0], input_[1])))

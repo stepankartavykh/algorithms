7625f58463ac4fab273009e8c7baf95d3756f7c6
sklion <sktvpx@gmail.com>
2024-02-17 10:51:35 +0300

    40 - done (TODO)

--- neetcode/Backtracking/40_combination_sum_second.py
+++ neetcode/Backtracking/40_combination_sum_second.py
@@ -31,6 +31,44 @@ def combination_sum_second(candidates: list[int], target: int) -> list[list[int]
     return result
 
 
+def combination_sum_not_my_solution(candidates: list[int], target: int) -> list[list[int]]:
+    # TODO Resolve later
+
+    # Sort array with numbers
+    candidates.sort()
+
+    result = []
+
+    # Define backtracking function
+    def backtrack(cur_subset, cur_position, cur_target):
+        # If cur_target is zero - add cur_subset. By the way [] not include in results due to constraints.
+        if cur_target == 0:
+            result.append(cur_subset.copy())
+            return
+        # If sum of elements > target, then we pass negative value to backtracking function, so we need to end this
+        # leave on this stage of decision tree.
+        if target <= 0:
+            return
+
+        # TODO This place with previous = -1 requires analysis.
+        previous = -1
+        for i in range(cur_position, len(candidates)):
+            # Comparison to avoid duplicates.
+            if candidates[i] == previous:
+                continue
+            # Add value to subset to jump inside this path.
+            cur_subset.append(candidates[i])
+            backtrack(cur_subset, i + 1, cur_target - candidates[i])
+            # TODO Why don't we call the function again after pop() from cur_subset?
+            cur_subset.pop()
+            # Store value we added to cur_subset.
+            previous = candidates[i]
+
+    backtrack([], 0, target)
+
+    return result
+
+
 if __name__ == '__main__':
     inputs = [
         [[5], 5],
@@ -38,4 +76,4 @@ if __name__ == '__main__':
         [[10, 1, 2, 7, 6, 1, 5], 8],
     ]
     for input_ in inputs:
-        print(combination_sum_second(input_[0], input_[1]))
+        print(combination_sum_not_my_solution(input_[0], input_[1]))

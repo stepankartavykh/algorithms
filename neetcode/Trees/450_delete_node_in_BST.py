70ca5e1c7290c8c221b2cd44452ab6b3108cde96
sklion <sktvpx@gmail.com>
2024-03-08 09:07:33 +0300

    450 - done (TODO - make iterative solution)

---
+++ neetcode/Trees/450_delete_node_in_BST.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+from neetcode import TreeNode, create_binary_tree_structure, print_binary_tree_structure
+
+
+def deleteNode(root: TreeNode | None, key: int) -> TreeNode | None:
+    if not root:
+        return root
+
+    if key < root.val:
+        root.left = deleteNode(root.left, key)
+    elif key > root.val:
+        root.right = deleteNode(root.right, key)
+    else:
+        if not root.left:
+            return root.right
+        elif not root.right:
+            return root.left
+
+        min_node_in_right = root.right
+        while min_node_in_right.left:
+            min_node_in_right = min_node_in_right.left
+
+        # TODO analyze this place again!
+        root.val = min_node_in_right.val
+        root.right = deleteNode(root.right, root.val)
+
+    return root
+
+
+def deleteNode_iterative(root: TreeNode | None, key: int) -> TreeNode | None:
+    stack = deque([root])
+
+    while stack:
+        current = stack.pop()
+
+
+if __name__ == '__main__':
+    inputs = [
+        ([2, 1, 3], 2),
+        ([5, 3, None, 2, 4], 3),
+        ([5, 3, 6, 2, 4, None, 7], 3),
+        ([5, 3, 6, 2, 4, None, 7], 0),
+    ]
+    for input_ in inputs:
+        tree = create_binary_tree_structure(input_[0])
+        key = input_[1]
+        print_binary_tree_structure(deleteNode(tree, key))

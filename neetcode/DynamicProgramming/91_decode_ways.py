8448bbaa18f30bebb4c6b4905cbd83a2195e2f7e
sklion <sktvpx@gmail.com>
2024-03-28 09:19:27 +0300

    91 - TODO - make solution with 2 variables in dp

--- neetcode/DynamicProgramming/91_decode_ways.py
+++ neetcode/DynamicProgramming/91_decode_ways.py
@@ -1,2 +1,63 @@
 def num_decodings(s: str) -> int:
-    pass
+    if len(s) == 0:
+        return 1
+    elif s[0] == '0':
+        return 0
+    elif len(s) == 1:
+        return 1
+    else:
+        if int(s[:2]) <= 26:
+            return num_decodings(s[1:]) + num_decodings(s[2:])
+        else:
+            return num_decodings(s[1:])
+
+
+def num_decodings_dp(s: str) -> int:
+    cached = {len(s): 1}
+
+    def number_of_ways_from_position(i):
+        if i in cached:
+            return cached[i]
+        if s[i] == '0':
+            return 0
+
+        current_number_ways = number_of_ways_from_position(i + 1)
+        if i + 1 < len(s) and (s[i] == '1' or (s[i] == '2' and s[i + 1] in '0123456')):
+            current_number_ways += number_of_ways_from_position(i + 2)
+        cached[i] = current_number_ways
+        return current_number_ways
+
+    return number_of_ways_from_position(0)
+
+
+def bruteforce(s: str) -> int:
+    # TODO Memory Limit Exceeded!
+    ways = []
+    current = []
+
+    def backtrack(i):
+        if i >= len(s):
+            ways.append(current.copy())
+            return
+
+        if s[i] in '123456789':
+            current.append(s[i])
+            backtrack(i + 1)
+            current.pop()
+            if i + 1 < len(s) and (s[i] == '1' or s[i] == '2' and s[i + 1] in '0123456'):
+                current.append(s[i] + s[i + 1])
+                backtrack(i + 2)
+
+    backtrack(0)
+
+    return len(ways)
+
+
+if __name__ == '__main__':
+    print(num_decodings_dp('111111111111111111111111111111111111111111111'))
+    print(num_decodings_dp('1'))
+    print(num_decodings_dp('12'))
+    print(num_decodings_dp('226'))
+    print(num_decodings_dp('06'))
+    print(num_decodings_dp('906'))
+    print(num_decodings_dp('12345'))

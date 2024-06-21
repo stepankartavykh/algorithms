7ef95f43e1fcb6dd524d7a6382fd65193549d2e7
sklion <sktvpx@gmail.com>
2024-06-21 08:57:06 +0300

    208 - TODO (TestClient for two classes)

--- neetcode/Tries/208_implement_trie_prefix_tree.py
+++ neetcode/Tries/208_implement_trie_prefix_tree.py
@@ -1,54 +1,80 @@
-class Trie:
-    def __init__(self, val=None):
-        self.val = val if val else ''
-        self.children = [None for _ in range(26)]
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 26
         self.end = False
 
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
     def insert(self, word: str) -> None:
-        if word:
-            first_letter = word[0]
-            pos: int = ord(first_letter) - ord('a')
-            if not self.children[pos]:
-                self.children[pos] = Trie(val=first_letter)
-            self.children[pos].insert(word[1:])
+        current = self.root
+        for symbol in word:
+            i = ord(symbol) - ord('a')
+            if current.children[i] is None:
+                current.children[i] = TrieNode()
+            current = current.children[i]
+        current.end = True
 
     def search(self, word: str) -> bool:
-        if word:
-            pos: int = ord(word[0]) - ord('a')
-            if self.children[pos]:
-                if len(word) == 1 and self.children[pos]:
-                    return True
-                inner = self.children[pos]
-                return inner.search(word[1:])
-            return False
-        return False
+        current = self.root
+        for symbol in word:
+            i = ord(symbol) - ord('a')
+            if current.children[i] is None:
+                return False
+            current = current.children[i]
+        return current.end
 
-    def startsWith(self, prefix: str) -> bool:
-        if prefix:
-            pos: int = ord(prefix[0]) - ord('a')
-            if self.children[pos]:
-                if len(prefix) == 1 and self.children[pos]:
-                    return True
-                inner = self.children[pos]
-                return inner.search(prefix[1:])
-            return False
-        return False
+    def starts_with(self, prefix: str) -> bool:
+        current = self.root
+        for symbol in prefix:
+            i = ord(symbol) - ord('a')
+            if current.children[i] is None:
+                return False
+            current = current.children[i]
+        current.end = True
+
+
+class TrieConciseApproach:
+
+    def __init__(self):
+        self.trie = {}
+        self.words = set()
+
+    def insert(self, word: str) -> None:
+        cur = self.trie
+        for c in word:
+            if c not in cur:
+                cur[c] = {}
+            cur = cur[c]
+        self.words.add(word)
+
+    def search(self, word: str) -> bool:
+        return word in self.words
+
+    def starts_with(self, prefix: str) -> bool:
+        cur = self.trie
+        for c in prefix:
+            if c not in cur:
+                return False
+            cur = cur[c]
+        return True
 
 
 if __name__ == '__main__':
     inputs = [
         (["insert", "search", "search", "startsWith", "insert", "search"], ["apple", "apple", "app", "app", "app", "app"]),
-        # (["insert", "search", "search", "search"], ["apple", "ap", "apple", 'qw']),
+        (["insert", "search", "search", "search"], ["apple", "ap", "apple", 'qw']),
     ]
     for input_ in inputs:
-        trie = Trie()
+        trie = TrieConciseApproach()
         values = input_[1]
         for index, operation in enumerate(input_[0]):
-            func = getattr(trie, operation)
             value = values[index]
             if operation == 'insert':
                 trie.insert(value)
             elif operation == 'search':
                 print(f'val = <{value}> in Trie - ' + f'{trie.search(value)}')
             elif operation == 'startsWith':
-                print(trie.startsWith(value))
+                print(trie.starts_with(value))

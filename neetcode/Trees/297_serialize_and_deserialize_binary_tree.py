1b4fc84822fedb63994d3df43e978d90e9f89ef8
sklion <sktvpx@gmail.com>
2024-08-15 09:26:04 +0300

    297 - commit for future comparison

--- neetcode/Trees/297_serialize_and_deserialize_binary_tree.py
+++ neetcode/Trees/297_serialize_and_deserialize_binary_tree.py
@@ -12,26 +12,50 @@ class Codec:
         if not root.left:
             return str(root.val) + '[][' + self.serialize(root.right) + ']'
         if not root.right:
-            return str(root.val) + '[' + self.serialize(root.left) + ']'
+            return str(root.val) + '[' + self.serialize(root.left) + '][]'
         return str(root.val) + f'[{self.serialize(root.left)}][{self.serialize(root.right)}]'
 
-    def deserialize(self, data: str) -> TreeNode:
+    def deserialize(self, data: str) -> TreeNode | None:
         # TODO - recursive? Find element and brackets '[' and ']' and then create left and right nodes?
         if data == '':
-            return TreeNode()
-        elements = data.split('[')
-        stack = []
-        result_tree = TreeNode(val=int(elements[0]))
-        current_node = result_tree
-        for element in elements[1:]:
-            if ']' not in element:
-                pass
-
+            return None
+        first_opening_bracket = data.find('[')
+        if first_opening_bracket == -1:
+            return TreeNode(int(data))
+        head_value = int(data[:first_opening_bracket])
+        diff = 0
+        flag = False
+        count_pairs = 0
+        end_index = None
+        for index in range(first_opening_bracket + 1, len(data)):
+            if data[index] == ']' and index == first_opening_bracket + 1:
+                end_index = index
+                break
+            elif data[index] == '[':
+                flag = True
+                diff += 1
+            elif data[index] == ']' and count_pairs == 0:
+                end_index = index
+                break
+            elif data[index] == ']':
+                diff -= 1
+            if flag is True and diff == 0:
+                count_pairs += 1
+            if count_pairs == 2:
+                end_index = index
+                break
+        result_tree = TreeNode(val=head_value)
+        left_node = self.deserialize(data[first_opening_bracket + 1:end_index + 1])
+        right_node = self.deserialize(data[end_index + 2:len(data) - 1])
+        result_tree.left = left_node
+        result_tree.right = right_node
         return result_tree
 
 
 if __name__ == '__main__':
-    tree = create_binary_tree_structure([13, 2345, 31, None, 12, 4, 5])
+    # tree = create_binary_tree_structure([13, 2345, 31, None, 12, 4, 5])
+    # tree = create_binary_tree_structure([13, 2345, 31, 1111, 12, 4])
+    tree = create_binary_tree_structure([1, 2, 3, 4, 5])
     codec = Codec()
     print(codec.serialize(tree))
     same_tree = codec.deserialize(codec.serialize(tree))

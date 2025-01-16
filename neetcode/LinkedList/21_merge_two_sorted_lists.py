3b373024a9129d135c0a35404d049144cec9bf74
sklion <sktvpx@gmail.com>
2025-01-16 08:59:56 +0300

    21 - time execution comparison

--- neetcode/LinkedList/21_merge_two_sorted_lists.py
+++ neetcode/LinkedList/21_merge_two_sorted_lists.py
@@ -1,3 +1,4 @@
+import timeit
 from typing import Optional
 
 
@@ -35,13 +36,33 @@ def merge_two_lists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Opt
 
 
 def second_recursion_approach_merge_two_lists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-    if list1 == None and list2 == None:
+    if list1 is None and list2 is None:
         return None
-    elif list1 == None:
+    elif list1 is None:
         return list2
-    elif list2 == None:
+    elif list2 is None:
         return list1
 
     if list1.val <= list2.val:
         return ListNode(list1.val, second_recursion_approach_merge_two_lists(list1.next, list2))
     return ListNode(list2.val, second_recursion_approach_merge_two_lists(list2.next, list1))
+
+
+if __name__ == '__main__':
+    first_program = '''
+import random
+from neetcode.LinkedList import create_linked_list_structure
+first = create_linked_list_structure([random.randint(-100, 100) for i in range(0, 100)])
+second = create_linked_list_structure([random.randint(-100, 100) for i in range(0, 400)])
+merge_two_lists(first, second)    
+    '''
+    second_program = '''
+import random
+from neetcode.LinkedList import create_linked_list_structure
+first = create_linked_list_structure([random.randint(-100, 100) for i in range(0, 100)])
+second = create_linked_list_structure([random.randint(-100, 100) for i in range(0, 400)])
+second_recursion_approach_merge_two_lists(first, second)    
+    '''
+    # TODO check memory consumption
+    print(timeit.timeit(first_program, number=10000, setup='from __main__ import merge_two_lists'))
+    print(timeit.timeit(second_program, number=10000, setup='from __main__ import second_recursion_approach_merge_two_lists'))
